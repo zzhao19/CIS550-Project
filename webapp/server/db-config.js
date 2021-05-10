83d5deabadff022029fb885333b3b4bef515b466
Ziyu Zhao <zzhao19@upenn.edu>
2021-05-10 01:54:20 -0700

    Delete webapp directory

--- webapp/server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-// Replace <Your Pennkey> and <Your Password>
-module.exports = {
-  host: "fling.seas.upenn.edu",
-  user: "zzhao19",
-  password: "",
-  database: "zzhao19"
-};
